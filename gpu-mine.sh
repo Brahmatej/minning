fef7fefcfe38dc34a92cf373e5ff857efa1dfd03
Brahmatej <37301787+Brahmatej@users.noreply.github.com>
2021-12-31 12:42:16 +0530

    Update gpu-mine.sh

--- gpu-mine.sh
+++ gpu-mine.sh
@@ -1,4 +1,3 @@
 wget https://github.com/cynixx3/third-party-miner-installer-for-ethos/releases/download/phoenixminer_5.7b/PhoenixMiner_5.7b_Linux.tar.xz
 tar -xf PhoenixMiner_5.7b_Linux.tar.xz
-cd PhoenixMiner_5.7b_Linux/
 sudo PhoenixMiner_5.7b_Linux/PhoenixMiner -pool minexmr.com:4444  -wal XMR:88YWTTJLVHSGC3hCvzTZ6zC23Bag4umiNWKnnFzVqQt2Lx1BMQwuar7KWnJfD2hgNcHEWyWoxu1YD3Gqf2XDwV2Z7dBUqKd.Rig001
